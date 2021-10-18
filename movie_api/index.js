3c8a69f861859f09f1a2db710810b6ce827b26a8
website-portfolio <Nannaphat.sudchawa@gmail.com>
2021-10-17 19:21:32 -0700

    Updated Environment Variables

--- movie_api/index.js
+++ movie_api/index.js
@@ -24,7 +24,7 @@ const Users = Models.User;
 const Genres = Models.Genre;
 const Directors = Models.Director;
 
-mongoose.connect('mongodb://localhost:27017/myFlix', {
+mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
